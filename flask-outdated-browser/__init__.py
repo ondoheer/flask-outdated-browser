9cbd364853a26ad94602a59f6f9fbd95a86d8021
Pedro Baumann <ondoheer@gmail.com>
2015-04-08 21:11:13 -0500

    scaffolding for the plugin done

---
+++ flask-outdated-browser/__init__.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+    flaskext.outdated-browser
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Adds outdatedbrowser easily to any flask project
+
+    :copyright: (c) 2015 by ondoheer.
+    :license: MIT, see LICENSE for more details.
+"""
+
+
+__version__ = '0.0.1'
+__version_info__ = __version__.split('.')
+__author__ = 'Pedro Baumann'
+__license__ = 'MIT'
+__copyright__ = '(c) 2015 - Pedro Baumann.'
+
+from flask import Blueprint
+
+
+class OutdatedBrowser(object):
+
+    """
+    :param: app: Flask aplication
+
+    """
+
+    def __init__(self,app=None):
+
+        if app:
+            self.init_app(app)
+
+
+
+    def init_app(self, app):
+
+        self.app = app
+
+        app.config.setdefault('OUTDATED_BROWSER_FOR', "IE10")
+        app.config.setdefault('OUTDATED_BROWSER_JQUERY', False)
+        app.config.setdefault('OUTDATED_BROWSER_AJAX', False)
+        app.config.setdefault('OUTDATED_BROWSER_MINIFIED', True)
+        app.config.setdefault('OUTDATED_BROWSER_LANGUAGE', 'en')
+
+        blueprint = Blueprint(
+            'outdated_browser',
+            __name__,
+            static_folder='static',
+            template_folder='templates',
+            static_url_path=app.static_url_path + '/outdated_browser',
+        )
+
+        app.register_blueprint(blueprint)
+
+
+
+
+
+        if not hasattr(app, 'extensions'):
+            app.extensions = {}
